f4193cc4c461bb4d3315c882dc65e83562a2b797
Simon Thörnqvist <simon@plug-n-trade.com>
2018-04-17 21:12:12 +0200

    limit output, set type to atom

--- apps/helay_client/lib/helay_client/transform.ex
+++ apps/helay_client/lib/helay_client/transform.ex
@@ -1,4 +1,5 @@
 defmodule HelayClient.Transform do
+  @derive {Poison.Encoder, only: [:args, :type]}
   defstruct type: nil, args: nil, input: nil, output: nil
 
   @type t :: %__MODULE__{
@@ -10,25 +11,27 @@ defmodule HelayClient.Transform do
 
   @callback run(t()) :: {:ok, t()} | {:error, term()}
 
-  @spec pipe(t(), term()) :: t()
-  def pipe(%__MODULE__{output: output} = t, args) do
-    t
-    |> Map.replace!(:args, args)
-    |> Map.put(:input, output)
-    |> Map.put(:output, nil)
+  def activate([%__MODULE__{} = h | t], input) do
+    [Map.put(h, :input, input) | t]
   end
 
-  def new(opts) when is_map(opts), do: to_struct(__MODULE__, opts)
+  def new(opts) when is_map(opts) do
+    opts = Map.update!(opts, "type", &(String.to_atom(&1)))
+
+    to_struct(__MODULE__, opts)
+  end
+
   def new(opts), do: struct(__MODULE__, opts)
 
   # https://stackoverflow.com/questions/30927635/in-elixir-how-do-you-initialize-a-struct-with-a-map-variable
   defp to_struct(kind, attrs) do
     struct = struct(kind)
-    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
+
+    Enum.reduce(Map.to_list(struct), struct, fn {k, _}, acc ->
       case Map.fetch(attrs, Atom.to_string(k)) do
         {:ok, v} -> %{acc | k => v}
         :error -> acc
       end
-    end
+    end)
   end
 end
